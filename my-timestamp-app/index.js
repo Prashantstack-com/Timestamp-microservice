e130ba0c96aa92518468bfac709656aae0c6ec3c
Prashant <mapraashant@gmail.com>
2026-01-31 12:53:47 +1100

    my-timestamp-app

--- my-timestamp-app/index.js
+++ my-timestamp-app/index.js
@@ -34,58 +34,6 @@ app.get("/api/whoami", function(req,res){
 })
 
 
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  const greeting = "THis is the first endpoint";
-  console.log(greeting);
-  res.json({greeting: 'hello API'});
-});
-//Step 1: Current Time Logic
-//  route to api that shows date in UTC
-app.get("/api", function(req,res){
-  const date = Date.now(); //gives date in number form 
-  
-  const newDate = new Date(date); // new Date creates date object 
-  res.json({
-    unix: date , //unix machine readable
-    utc: newDate.toUTCString()    //utc human readable
-  })
-});
-
-//Step 2: String Date Parsing
-//  route to api that handle valid date string parse
-app.get("/api/:date", function(req, res){
-
-const dateId = req.params.date;
-let finalDate;
-
-// 1. The Test: Is the entire string just digits?
-if (/^\d+$/.test(dateId)) {
-  // 2. If numeric, convert to a Number
-  finalDate = new Date(Number(dateId)); //store as a obj so json format is possible
-} else {
-  // 3. Otherwise, treat as a date string
-  finalDate = new Date(dateId);
-}
-
-// 4. Send the result using our finalDate object
-// Step 4 Logic: Error Handling
-  if (finalDate.toString() === "Invalid Date") {
-    res.json({ error: "Invalid Date" });
-  } else {
-    //getTime() a larger number in milliseconds
-res.json({ 
-  unix: finalDate.getTime(), 
-  utc: finalDate.toUTCString() 
-});
-};
-
-});
-
-
-
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
